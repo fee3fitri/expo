be80765eb3aeb86b6fd4baf22a09dc41fefdffc9
fee3fitri <safitri.shelton@gmail.com>
2023-01-19 15:14:04 -0800

    Fundamental

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Expo...</Text>
+      <Text style={[styles.title, {flex: 5}]}>Step One</Text>
+      <Text style={[styles.title, {flex: 3}]}>Step Two</Text>
+      <Text style={[styles.title, {flex: 4}]}>Step Three</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +15,17 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 40,
+    backgroundColor: 'orange',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: 'blue ',
+    backgroundColor: 'gray',
+    margin: 10,
+    padding: 20,
+  }
 });
